8830615c381fbbf07cae3a2edebea746e351a16e
atharvas2003 <sawantatharva@protonmail.com>
2026-07-03 16:03:41 +0530

    Allow frontend CORS

--- quiz-backend/app/main.py
+++ quiz-backend/app/main.py
@@ -17,12 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
