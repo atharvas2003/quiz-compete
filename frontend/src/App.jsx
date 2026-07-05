a4570a2fdf5363a7df4d0f11cdd55722a42f3c46
atharvas2003 <sawantatharva@protonmail.com>
2026-07-05 22:49:42 +0530

    added NotFound again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -107,7 +107,6 @@ function App() {
                     </ProtectedRoute>
                 }
             />
-
             <Route path="*" element={<NotFound />} />
             <Route
     path="*"
